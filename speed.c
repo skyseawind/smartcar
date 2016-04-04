7166edf0f956ea61a6bdb1758a694cfc4777c087
skyseawind <2985080396@qq.com>
2016-04-04 20:13:56 +0800

    学习git

--- speed.c
+++ speed.c
@@ -1,7 +1,7 @@
-
+/**************yangxiao************/
 #include "common.h"
 #include "include.h"
-
+//杨逍修改实验git
 void PIT0_IRQHandler(void)
 {
     if(PIT_TFLG(QEP_pit) == 1)                         //判断是否 PIT0 进入中断
